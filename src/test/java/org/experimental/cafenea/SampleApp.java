8b1853d0a16153606786b8ac2ba4881c221c91d5
Ruslan <masquerade2ruslan@gmail.com>
2018-04-24 00:57:50 -0400

    docs

--- src/test/java/org/experimental/cafenea/SampleApp.java
+++ src/test/java/org/experimental/cafenea/SampleApp.java
@@ -7,6 +7,34 @@ import org.testng.annotations.Test;
 
 import java.util.UUID;
 
+
+/*
+
+customer
+	send(NewOrder -> [barsita])
+
+	on PaymentDue
+		reply(SubmitPayment)
+	on DrinkReady
+		end.
+
+cashier
+	on NewOrder
+		Publish(PrepareDrink)
+		Reply(PaymentDue)
+
+	on SubmitPayment
+		Publish(PaymentComplete)
+
+barista
+	on PrepareDrink
+		publish(DrinkReady)
+	on PaymentComplete
+		publish(DrinkReady)
+
+ */
+
+
 public class SampleApp extends Env {
 
     ////////// barista
